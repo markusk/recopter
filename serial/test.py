91b09f8d9d8fa575159c81ae8af48bd14a6b1d35
Markus Knapp <bitbucket@direcs.de>
2013-07-01 23:13:15 +0200

    initial commit

---
+++ serial/test.py
@@ -0,0 +1,11 @@
+import serial
+ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
+ser.open()
+
+ser.write("testing")
+try:
+        while 1:
+                response = ser.readline()
+                print response
+except KeyboardInterrupt:
+        ser.close()
